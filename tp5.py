b944b920e82972884701e39d8392c29353dbc215
Shcherbakova Iuliia <yuliyanasher@gmail.com>
2016-03-10 17:23:23 +0100

    Create tp5.py

---
+++ tp5.py
@@ -0,0 +1,51 @@
+display = __name__ == '__main__'
+def partie(name):
+    if display:
+        print('\n***', 'Partie', name, '***')
+
+partie("Le thème du TP")
+
+print("-l : afficher le nombre de lignes")
+print("24 cigale.txt")
+print("-w : afficher le nombre de mots")
+print("108 cigale.txt")
+print("-c : afficher le nombre d’octets")
+print("639 cigale.txt")
+print("-lw : afficher le nombre de lignes et de mots")
+print("24 108 cigale.txt")
+print("-lc : afficher le nombre de lignes et d’octets")
+print("24 639 cigale.txt")
+print("-wc : afficher le nombre de mots et d’octets")
+print("108 639 cigale.txt")
+print("-lwc : afficher le nombre de lignes, de mots et d’octets")
+print("24 108 639 cigale.txt")
+print("Pour le vide.txt 0, 00, ou 000 partout")
+
+partie("Programmation")
+
+SEPARATEURS = "\n  \t     . ? ! , ; : - _ ' '' () [] {} + - * / = < >"
+
+        
+def decoupe_en_mots(chaine):
+    return chaine.split(' ')
+
+myfile = open('cigale.txt')  
+lines = myfile.readlines()  
+list_of_elements = []
+for line in lines:
+    nline=''
+    for i in range(len(line)):
+        if line[i] in SEPARATEURS:
+            nline+=' '
+        else:
+            nline+=line[i]
+    list_of_elements += nline.split()
+myfile.close()
+print(list_of_elements)
+print(len(list_of_elements))
+
+def analyse(canal):
+    myfile = open(canal)
+    l = sum(1 for line in myfile)
+    m = len(list_of_elements)
+    print(l,m)
