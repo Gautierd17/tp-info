188eefc23b91203279f5a7a9605affd3cc581012
Iuliia <yuliyanasher@gmail.com>
2016-10-04 14:20:07 +0200

    Update turtle.py

--- L2S3/turtle.py
+++ L2S3/turtle.py
@@ -24,3 +24,25 @@ def vk_fl(l,n):
     for i in range(3):
         vk(l,n)
         right(120)
+
+import turtle
+def draw_sierpinski(length,depth):
+    if depth==0:
+        for i in range(0,3):
+            t.fd(length)
+            t.left(120)
+    else:
+        draw_sierpinski(length/2,depth-1)
+        t.fd(length/2)
+        draw_sierpinski(length/2,depth-1)
+        t.bk(length/2)
+        t.left(60)
+        t.fd(length/2)
+        t.right(60)
+        draw_sierpinski(length/2,depth-1)
+
+
+window = turtle.Screen()
+t = turtle.Turtle()
+draw_sierpinski(500,1)
+window.exitonclick()
