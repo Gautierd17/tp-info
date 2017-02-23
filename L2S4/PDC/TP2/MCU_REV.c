2339c55b11c335d1e0aebf4dc68f9ee6fc72be08
Yulia <yuliyanahser@gmail.com>
2017-02-23 16:13:45 +0100

    Remove Folder TP4

--- L2S4/PDC/TP2/MCU_REV.c
+++ L2S4/PDC/TP2/MCU_REV.c
@@ -1,17 +1,44 @@
+#include <stdio.h>
 #include "mcu_macro.h"
 #include "mcu_readl.h"
+/*mcu_wc*/
 
 int line[MAXLINE+1] ;
-
 int
 main
 (void)
 {
-	int item, lines;
-	int lines=0; 
+  int count=0;
+  while(1){
+  count+=readl(line);
+  strrev(count);
+  if (count==EOF) putchar(count) ; 
+  }
+  return 0;
+}
 
-	while((item=getchar()) != EOF){
-		putchar(strrev(lines));
-	}
-	return 0;
+#include <stdio.h>
+
+#define Num   MAXLINE
+
+int main( void ) 
+{
+    char str[Num], revstring[Num];
+
+    
+    getchar(str, Num, stdin);
+
+    size_t  length = 0;
+    while (str[length] != '\0' && str[length] != '\n') ++length;
+
+    if (str[length] == '\n') str[length] = '\0';
+
+    size_t i = 0;
+    for (i != length; i++) revstring[i] = str[length - i - 1];
+
+    revstring[i] = '\0';
+
+    putchar(revstring);
+
+    return 0;
 }
