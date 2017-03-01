b7b9f3e920ed219241d01b1f266ab1c54e2b2ef1
Iuliia <yuliyanasher@gmail.com>
2017-03-01 16:53:09 +0100

    Update txt.sql

--- L2S4/TW2/SQL_FILES/file/txt.sql
+++ L2S4/TW2/SQL_FILES/file/txt.sql
@@ -13,3 +13,25 @@ WHERE  equipe='LavePlusBlanc'
 SELECT CONCAT("nom","equipe") FROM coureurs;
 
 UPPER/LOWER 
+
+Question 1.3
+
+SELECT coureurs.dossard, coureurs.nom
+FROM coureurs
+WHERE coureurs.nom LIKE 'a%'
+
+SELECT coureurs.dossard, coureurs.nom
+FROM coureurs
+WHERE coureurs.nom LIKE '%er%'
+
+SELECT coureurs.dossard, coureurs.nom
+FROM coureurs
+WHERE coureurs.nom LIKE '_____'
+
+SELECT coureurs.dossard, coureurs.nom
+FROM coureurs
+WHERE coureurs.nom LIKE '%a__'
+
+SELECT coureurs.dossard, coureurs.nom
+FROM coureurs
+WHERE coureurs.nom LIKE '%a__%'
